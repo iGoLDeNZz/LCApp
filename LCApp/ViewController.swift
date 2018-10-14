c7ff7af810e681e08385feac41f5214c977ba714
Yousef <yousef1415@hotmail.com>
2018-10-14 03:37:52 +0300

    clean project

---
+++ LCApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LCApp
+//
+//  Created by Yousef At-tamimi on 10/14/18.
+//  Copyright © 2018 Law Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
